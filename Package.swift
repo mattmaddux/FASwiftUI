7805321730e28d3b0a73e9c86abbd00500eb29f5
Matt Maddux <mattmaddux@gmail.com>
2019-10-04 15:19:20 -0500

    Edited Package.swift

--- Package.swift
+++ Package.swift
@@ -22,9 +22,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FASwiftUI",
-            dependencies: []),
-        .testTarget(
-            name: "FASwiftUITests",
-            dependencies: ["FASwiftUI"]),
+            dependencies: ["QGrid"]),
     ]
 )
